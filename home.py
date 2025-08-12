985c04f5446e63083a4fd1f5937e82e459114a5c
Rxz1Eaco <eacorocha@gmail.com>
2025-08-12 17:52:36 -0300

    Primeiro commit

---
+++ home.py
@@ -0,0 +1,14 @@
+import streamlit as st 
+
+
+st.title('Projeto Amon')
+st.subheader('Machine Learning Apps')
+
+
+st.sidebar.markdown('Desenvolvido por [Éaco Rocha](www.google.com.br)')
+st.write('''
+
+
+
+
+''')
